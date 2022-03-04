971320d9f739b9702a12b62180e137cb31249a5a
filipeas <felipealvessampaio@hotmail.com>
2022-03-03 21:24:12 -0300

    implementacao do sistema de controle de loop no menu principal #2

--- Main.java
+++ Main.java
@@ -3,11 +3,30 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
+		int opc = 0;
+		do {
+			Menu mainMenu = new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
 
-		int opc = mainMenu.getSelection();
+			opc = mainMenu.getSelection();
 
-		System.out.println(opc + " foi selecionada");
+			switch (opc) {
+				case 1:
+					// chama classe de contas
+					break;
+				case 2:
+					// chama classe de clientes
+					break;
+				case 3:
+					// chama classe de operações
+					break;
+				case 4:
+					continue;
+				default:
+					System.out.println("Opção inválida para o menu principal...");
+					break;
+			}
+		} while (opc != 4);
+
 		System.out.println("Fim");
 	}
 
